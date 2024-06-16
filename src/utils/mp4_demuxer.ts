8bb4128cdf3e98e5d84b689878df7367db58395f
Sam Hobson <shhobson@protonmail.com>
2024-06-17 09:09:14 +1000

    Added mp4 demuxer

--- src/utils/mp4_demuxer.ts
+++ src/utils/mp4_demuxer.ts
@@ -1,4 +1,4 @@
-import type { MP4ArrayBuffer, MP4File } from "mp4box";
+import { DataStream, createFile, type MP4ArrayBuffer, type MP4File, type MP4Info, type MP4VideoTrack, type Sample } from "mp4box";
 
 class Mp4FileSink implements UnderlyingSink {
     _file: MP4File
@@ -26,4 +26,74 @@ class Mp4FileSink implements UnderlyingSink {
         // this._setStatus("fetch", "Done");
         this._file.flush();
     }
+}
+
+function description(track: MP4VideoTrack, file: MP4File) {
+    // Get the appropriate `description` for a specific track. Assumes that the
+    // track is H.264, H.265, VP8, VP9, or AV1.
+    const trak = file.getTrackById(track.id);
+
+    for (const entry of trak.mdia.minf.stbl.stsd.entries) {
+        const box = entry.avcC || entry.hvcC || entry.vpcC || entry.av1C;
+
+        if (box) {
+            const stream = new DataStream(undefined, 0, DataStream.BIG_ENDIAN);
+            box.write(stream);
+            // Remove the box header.
+            return new Uint8Array(stream.buffer, 8);
+        }
+    }
+
+    throw new Error("avcC, hvcC, vpcC, or av1C box not found");
+}
+
+function onReady(info: MP4Info, file: MP4File, onConfig: (config: VideoDecoderConfig) => void): void {
+    // TODO: This
+    // this._setStatus("demux", "Ready");
+
+    const track = info.videoTracks[0];
+
+    // Generate and emit an appropriate VideoDecoderConfig.
+    onConfig({
+        // Browser doesn't support parsing full vp8 codec (eg: `vp08.00.41.08`),
+        // they only support `vp8`.
+        codec: track.codec.startsWith('vp08') ? 'vp8' : track.codec,
+        codedHeight: track.video.height,
+        codedWidth: track.video.width,
+        description: description(track, file),
+    });
+
+    // Start demuxing.
+    file.setExtractionOptions(track.id);
+    file.start();
+}
+
+function onSamples(samples: Sample[], onChunk: (chunk: EncodedVideoChunk) => void): void {
+    // Generate and emit an EncodedVideoChunk for each demuxed sample.
+    for (const sample of samples) {
+        onChunk(new EncodedVideoChunk({
+            type: sample.is_sync ? "key" : "delta",
+            timestamp: 1e6 * sample.cts / sample.timescale,
+            duration: 1e6 * sample.duration / sample.timescale,
+            data: sample.data
+        }));
+    }
+}
+
+
+export function mp4Demux(
+    stream: ReadableStream,
+    onConfig: (config: VideoDecoderConfig) => void,
+    onChunk: (chunk: EncodedVideoChunk) => void,
+) {
+    // Configure an MP4Box File for demuxing.
+    const file = createFile();
+
+    // TODO: Set
+    // file.onError = error => setStatus("demux", error);
+    file.onReady = info => onReady(info, file, onConfig);
+    file.onSamples = (_1, _2, samples) => onSamples(samples, onChunk);
+
+    const sink = new Mp4FileSink(file);
+    stream.pipeTo(new WritableStream(sink, { highWaterMark: 2 }))
 }
